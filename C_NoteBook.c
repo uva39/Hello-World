f1cadd537b12bf27c7dc45a72f2696eabbc116e8
uva39 <36david07@gmail.com>
2022-10-19 18:06:51 +0900

    new

--- C_NoteBook.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define _CRT_SECURE_NO_WARNINGS
-
-
-int main(void){
-    
-    return 0;
-}
--- NoteBook.py
+++
@@ -1,4 +0,0 @@
-print("Hello, World!")
-
-for i in range(10):
-    print("asdsadsadsa")
