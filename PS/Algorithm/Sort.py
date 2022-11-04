7b1e7ca78d755022440b885d8be17d20ed263f35
36david07 <36david07@gmail.com>
2022-11-04 13:34:00 +0900

    new

--- PS/Algorithm/Sort.py
+++ PS/Algorithm/Sort.py
@@ -19,7 +19,7 @@ def selection(arr):
     for i in range(len(arr)):
         min_idx = i
         for j in range(i + 1, len(arr)):
-            if arr[i] > arr[j]:
+            if arr[min_idx] > arr[j]:
                 min_idx = j
         arr[min_idx], arr[i] = arr[i], arr[min_idx]
 
