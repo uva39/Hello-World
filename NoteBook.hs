95b946c2c93e9eac020c9e5fb9e8eefd85b3d8bb
36david07 <36david07@gmail.com>
2022-11-15 13:14:54 +0900

    new

--- NoteBook.hs
+++ NoteBook.hs
@@ -1,5 +1,7 @@
 f [] = []
-f (x:xs) = f ys ++ [x] ++ zs
-           where
-             ys = [a | a <- xs, a <= x]
-             zs = [b | b <- xs, b > x]
+f (x:xs) = f ys ++ [x] ++ zs -- 퀵소트 잘못된 구현임
+          where
+            ys = [a | a <- xs, a <= x]
+            zs = [b | b <- xs, b > x]
+
+factorial n = product [1..n]
